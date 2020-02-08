00a6b5fac20de854288594ea12313694b6ea9830
Polina <polina.j4@gmail.com>
2020-02-09 01:16:39 +0500

    Add files

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-var TelegramBot = require("node-telegram-bot-api"),
-	telegram = new TelegramBot("892772497:AAH2dQzRHp9kKBPGw7bBtWygbcmTx7Gf6jM", { polling: true });
+var TelegramBot = require('node-telegram-bot-api'),
+    telegram = new TelegramBot("892772497:AAH2dQzRHp9kKBPGw7bBtWygbcmTx7Gf6jM", { polling: true });
 
 telegram.on("text", (message) => {
-	telegram.sendMessage(message.chat.id, "Hello world");
+  telegram.sendMessage(message.chat.id, "Hello world");
 });
