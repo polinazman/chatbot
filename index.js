5ea93a9c59802bdd67fa3d483d9701c310d6978b
Polina <polina.j4@gmail.com>
2020-02-09 02:04:15 +0500

    Index file modified

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const Telegraf = require("telegraf");
-const bot = new Telegraf(process.env."892772497:AAH2dQzRHp9kKBPGw7bBtWygbcmTx7Gf6jM");
+const bot = new Telegraf("892772497:AAH2dQzRHp9kKBPGw7bBtWygbcmTx7Gf6jM");
 
-bot.on("text", (ctx) =>
-	username = telegram.getChatMember(chatId, userId) => Promise
-	ctx.reply("Hey there", username));
+bot.on("text", (ctx) =>{
+	return ctx.reply("Hey there");
+});
 
 bot.launch();
-
